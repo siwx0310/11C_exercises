dff9c1271b29c718c68b71eabe88da03c821d4be
Siw <siwx0310@stud.kea.dk>
2021-02-09 22:47:56 +0100

    template exercise

---
+++ template.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//Strings 5.3 small "template literals" exercise
+const nameExercise1 = "Siw";
+const animalExercise1 = "horse";
+const animalNameExercise1 = "Rocky";
+
+const textExercise1 = `My name is ${nameExercise1}.
+I have a ${animalExercise1} called ${animalNameExercise1}.`;
+
+console.log(textExercise1);
