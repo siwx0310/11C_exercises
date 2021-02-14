ab37f3e993096a713b4888f8bab050eba5d92655
Siw <siwx0310@stud.kea.dk>
2021-02-14 14:56:26 +0100

    exercise complete

--- basic_objects_compare.js
+++ basic_objects_compare.js
@@ -1,1 +1,32 @@
 "use strict";
+
+const student1 = {
+  firstName: "Harry",
+  lastName: "Potter",
+};
+
+const student2 = {
+  firstName: "Herry",
+  lastName: "Potter",
+};
+
+if (student1 == student2) {
+  console.log("the students are the same!");
+} else {
+  console.log("the students are not the same...");
+}
+
+student1.firstName = "siw";
+console.log(student1);
+console.log(student2);
+
+student1.firstName = student2.firstName;
+student1.lastName = student2.lastName;
+
+console.log(student1);
+console.log(student2);
+if (student1 == student2) {
+  console.log("the students are the same!");
+} else {
+  console.log("the students are not the same...");
+}
--- objects_combination.js
+++ objects_combination.js
@@ -1,1 +1,26 @@
 "use strict";
+
+// objects inside of objects
+
+const color = {
+  hex: "#c0ffee",
+  rgb: { r: 192, g: 255, b: 238 },
+  hsl: { h: 164, s: 100, l: 88 },
+};
+
+let red = color.rgb.r;
+console.log(`Red is: ${red}`);
+
+// objects and prototype
+
+const Animal = {
+  name: "",
+  type: "unknown",
+  desc: "",
+  age: 0,
+};
+
+const animal = Object.create(Animal);
+Animal.image = "image.jpg";
+
+console.log(animal.image);
